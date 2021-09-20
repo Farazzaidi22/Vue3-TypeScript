f17b1f42b627d8b0a7e303a7a62e1c163705578b
Faraz Khalid <farazkhalid05@yahoo.com>
2021-09-20 16:37:08 +0500

    options API setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     Hello Hyrule
+    <p>{{ name }} - {{age}}</p>
+    <button @click="changeName('Amil')">change name</button>
+    <button @click="changeAge('35')">change age</button>
+
   </div>
 </template>
 
@@ -10,6 +14,24 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'App',
   components: {
+  },
+
+  data(){
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+
+  methods: {
+    changeName(name: string){
+      this.name = name
+      return name
+    },
+    changeAge(age: number | number){
+      this.age = age
+      return age
+    }
   }
 });
 </script>
